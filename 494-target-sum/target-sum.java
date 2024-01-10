7e848dc086cba661d529ac3a45d20a0538113555
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-10 07:43:04 +0530

    Time: 5 ms (89.17%) | Memory: 42.9 MB (53.09%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,19 +1,46 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, 0, 0, target);
-    }
+        int sum = 0;
+        for(int i : nums)
+            sum+= i;
+        if(sum-target<0 || (sum-target)%2==1)
+            return 0;
+        int tar = (sum-target)/2;
+        int n = nums.length;
+        int[][] dp = new int[n][tar+1];
 
-    private int helper(int[] nums, int index, int curSum, int target) {
-        if (index == nums.length) {
-            if (curSum == target) {
-                return 1; 
-            } else {
-                return 0; 
+        if(nums[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+        if(nums[0] <=  tar && nums[0] != 0)
+            dp[0][nums[0]] = 1;
+        
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<=tar; j++){
+                int notTake = dp[i-1][j];
+                int take = 0;
+                if(nums[i] <= j)
+                    take = dp[i-1][j - nums[i]];
+                dp[i][j] = take+notTake;
             }
         }
-        
-        int left = helper(nums, index + 1, curSum + nums[index], target); 
-        int right = helper(nums, index + 1, curSum - nums[index], target);
-        return left + right; 
+        return dp[n-1][tar];
     }
+    //     return helper(nums, 0, 0, target);
+    // }
+
+    // private int helper(int[] nums, int index, int curSum, int target) {
+    //     if (index == nums.length) {
+    //         if (curSum == target) {
+    //             return 1; 
+    //         } else {
+    //             return 0; 
+    //         }
+    //     }
+        
+    //     int left = helper(nums, index + 1, curSum + nums[index], target); 
+    //     int right = helper(nums, index + 1, curSum - nums[index], target);
+    //     return left + right; 
+    // }
 }
