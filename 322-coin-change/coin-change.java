0adbc9bc3edeb0ca30655b57952bc4dbd2f91162
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-10 08:36:24 +0530

    Time: 16 ms (66.04%) | Memory: 45 MB (13.38%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,26 +3,45 @@ class Solution {
         int n = coins.length;
         // Arrays.sort(coins);
         int[][] dp = new int[n][amount+1];
-        for(int[] i : dp)
-            Arrays.fill(i, -1);
-        int a = helper(n-1, amount, coins, dp);
-        return a==1000000007?-1:a;
-    }
-    private static int helper(int n, int amount, int[] coins, int[][] dp){
-        if(amount == 0)
-            return 0;
-        if(n==0)
-            return amount%coins[n] == 0? amount/coins[n]:1000000007;
+        for(int i = 0; i<=amount; i++){
+            if(i%coins[0] == 0)
+                dp[0][i] = i/coins[0];
+            else
+                dp[0][i] = 1000000007;
+        }
 
-        if(dp[n][amount] != -1)
-            return dp[n][amount];
+        for(int i = 1; i<n; i++){
+            for(int tar = 0; tar<=amount; tar++){
+                int take = Integer.MAX_VALUE;
+                if(coins[i] <= tar)
+                    take = 1 + dp[i][tar- coins[i]];
+                
+                int notTake = dp[i-1][tar];
+                dp[i][tar] = Math.min(take, notTake);
+            }
+        }
+        return dp[n-1][amount]==1000000007?-1:dp[n-1][amount];
+    }
+    //     for(int[] i : dp)
+    //         Arrays.fill(i, -1);
+    //     int a = helper(n-1, amount, coins, dp);
+    //     return a==1000000007?-1:a;
+    // }
+    // private static int helper(int n, int amount, int[] coins, int[][] dp){
+    //     if(amount == 0)
+    //         return 0;
+    //     if(n==0)
+    //         return amount%coins[n] == 0? amount/coins[n]:1000000007;
+
+    //     if(dp[n][amount] != -1)
+    //         return dp[n][amount];
     
-        int take = Integer.MAX_VALUE;
-        if(coins[n] <= amount)
-            take = 1 + helper(n, amount-coins[n], coins, dp);
+    //     int take = Integer.MAX_VALUE;
+    //     if(coins[n] <= amount)
+    //         take = 1 + helper(n, amount-coins[n], coins, dp);
         
-        int notTake = helper(n-1, amount, coins, dp);
-        return dp[n][amount] = Math.min(take, notTake);
+    //     int notTake = helper(n-1, amount, coins, dp);
+    //     return dp[n][amount] = Math.min(take, notTake);
 
-    }
+    // }
 }
