5b1f6b296eea4ecf4cc6dc1c63c0ee082b977e6b
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-13 23:33:36 +0530

    Time: 18 ms (94.14%) | Memory: 44.8 MB (93.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,7 +2,6 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-         // int[][] dp = new int[m+1][n+1];
         int[] curr = new int[n+1];
         int[] prev = new int[n+1];
         for( int i = 1; i<=m; i++){
@@ -17,10 +16,25 @@ class Solution {
         }
         return prev[n];
     }
+
+    //     int[][] dp = new int[m+1][n+1];
+    //     for(int i = 1; i<=m; i++){
+    //         for(int j = 1; j<=n; j++){
+    //             if(text1.charAt(i-1) == text2.charAt(j-1))
+    //                 dp[i][j] = 1 + dp[i-1][j-1];
+    //             else
+    //                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[m][n];
+    // }
+
+
     //     for(int[] i : dp)y
     //         Arrays.fill(i, -1);
     //     return helper(m-1, n-1, text1, text2, dp);
     // }
+
     // private static int helper(int m, int n, String t1, String t2, int[][] dp){
     //     if(m<0||n<0)
     //         return 0;
