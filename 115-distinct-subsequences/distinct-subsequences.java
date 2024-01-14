71aeaaac3f5b4c642aa65401754b0e0018665834
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-14 19:48:09 +0530

    Time: 14 ms (72.31%) | Memory: 49.1 MB (18.17%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,22 +4,34 @@ class Solution {
         int n = t.length();
         if(m<n)
             return 0;
-        int[][] dp = new int[m][n];
-        for(int[] i : dp)
-            Arrays.fill(i, -1);
-        return helper(m-1, n-1, s, t, dp);
-    }
-    private static int helper(int i, int j, String s, String t, int[][] dp){
-        if(j<0)
-            return 1;
-        if(i<0)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
+        int[][] dp = new int[m+1][n+1];
+        for(int i = 0; i<=m; i++)
+            dp[i][0] = 1;
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                if(s.charAt(i-1) == t.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
         }
-        else
-            return dp[i][j] = helper(i-1, j, s, t, dp);
+        return dp[m][n];
     }
+    //     for(int[] i : dp)
+    //         Arrays.fill(i, -1);
+    //     return helper(m, n, s, t, dp);
+    // }
+    // private static int helper(int i, int j, String s, String t, int[][] dp){
+    //     if(j==0)
+    //         return 1;
+    //     if(i==0)
+    //         return 0;
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if(s.charAt(i-1) == t.charAt(j-1)){
+    //         return dp[i][j] = helper(i-1, j-1, s, t, dp) + helper(i-1, j, s, t, dp);
+    //     }
+    //     else
+    //         return dp[i][j] = helper(i-1, j, s, t, dp);
+    // }
 }
