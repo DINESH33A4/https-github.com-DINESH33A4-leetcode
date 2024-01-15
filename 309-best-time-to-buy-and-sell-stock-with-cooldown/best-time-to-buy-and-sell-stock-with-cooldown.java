06e201843af90a6e76a307cda7100c9a805ee0fe
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-15 19:43:09 +0530

    Time: 1 ms (84.98%) | Memory: 41.7 MB (28.50%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,23 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] i : dp)
-            Arrays.fill(i, -1);
-        return helper(0, 1, prices, dp);
-    }
-    private static int helper(int i, int buy, int[] prices, int[][] dp){
-        if(i>=prices.length)
-            return 0;
-        if(dp[i][buy] != -1)
-            return dp[i][buy];
-        if(buy == 1){
-            return dp[i][buy] = Math.max(-prices[i] + helper(i+1, 0, prices, dp), 
-                                               helper(i+1, 1, prices, dp));
+        int[][] dp = new int[n+2][2];
+        for(int i = n-1; i>=0; i--){
+            dp[i][0] = Math.max(prices[i] + dp[i+2][1], dp[i+1][0]);
+            dp[i][1] = Math.max(-prices[i] + dp[i+1][0], dp[i+1][1]);
         }
-        else{
-            return dp[i][buy] = Math.max(prices[i] + helper(i+2, 1, prices, dp),
-                                              helper(i+1, 0, prices, dp));
-        }
+        return dp[0][1];
     }
+    //     for(int[] i : dp)
+    //         Arrays.fill(i, -1);
+    //     return helper(0, 1, prices, dp);
+    // }
+    // private static int helper(int i, int buy, int[] prices, int[][] dp){
+    //     if(i>=prices.length)
+    //         return 0;
+    //     if(dp[i][buy] != -1)
+    //         return dp[i][buy];
+    //     if(buy == 1){
+    //         return dp[i][buy] = Math.max(-prices[i] + helper(i+1, 0, prices, dp), 
+    //                                            helper(i+1, 1, prices, dp));
+    //     }
+    //     else{
+    //         return dp[i][buy] = Math.max(prices[i] + helper(i+2, 1, prices, dp),
+    //                                           helper(i+1, 0, prices, dp));
+    //     }
+    // }
 }
