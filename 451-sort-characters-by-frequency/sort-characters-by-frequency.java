dfc9dc71c226ac80f4ccf3cb9ce717d6b4fdc919
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-21 17:07:23 +0530

    Time: 19 ms (39.60%) | Memory: 45.5 MB (41.43%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for(int i  = 0; i<s.length(); i++)  
+            freq.put(s.charAt(i), freq.getOrDefault(s.charAt(i), 0)+1);
+        PriorityQueue<mapping> pq = new PriorityQueue<>((a,b)->Integer.compare(b.freq, a.freq));
+        for(Character i : freq.keySet())
+            pq.add(new mapping(i, freq.get(i)));
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            mapping m = pq.poll(); 
+            sb.append(String.valueOf(m.ch).repeat(m.freq));
+        }
+        return sb.toString();
+    }
+}
+class mapping{
+    Character ch;
+    int freq;
+    public mapping(Character ch, int freq){
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
