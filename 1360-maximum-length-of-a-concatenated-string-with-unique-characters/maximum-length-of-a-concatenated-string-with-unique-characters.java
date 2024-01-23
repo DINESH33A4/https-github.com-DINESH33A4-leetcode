65e0e5d5a5bd0a593820f2ee29a69e1b0cf5191c
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-23 12:45:18 +0530

    Time: 72 ms (13.31%) | Memory: 52.9 MB (5.14%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,32 +1,28 @@
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
     public int maxLength(List<String> arr) {
-        return backtrack(arr, 0, "");
+        HashMap<String, Integer> m = new HashMap<>();
+        return helper(0, arr, "", m);
     }
-
-    private int backtrack(List<String> arr, int index, String current) {
-        if (!isUnique(current)) {
+    private static int helper(int idx, List<String>arr, String dup,HashMap<String,Integer> m){
+        boolean checked = check(dup);
+        if(!checked)
             return 0;
-        }
-
-        int maxLen = current.length();
-
-        for (int i = index; i < arr.size(); i++) {
-            maxLen = Math.max(maxLen, backtrack(arr, i + 1, current + arr.get(i)));
-        }
-
-        return maxLen;
+        if(idx == arr.size())
+            return checked?dup.length():0;
+        if(m.containsKey(idx+dup))
+            return m.get(idx+dup);
+        m.put(idx+dup, Math.max(helper(idx+1, arr, dup+arr.get(idx), m),
+                                helper(idx+1, arr, dup, m)));
+        return m.get(idx+dup);
     }
 
-    private boolean isUnique(String s) {
+     private static boolean check(String s){
         HashSet<Character> set = new HashSet<>();
-        for (char c : s.toCharArray()) {
-            if (!set.add(c)) {
+        for(int i = 0; i<s.length(); i++)
+            if(!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else
                 return false;
-            }
-        }
         return true;
     }
 }
