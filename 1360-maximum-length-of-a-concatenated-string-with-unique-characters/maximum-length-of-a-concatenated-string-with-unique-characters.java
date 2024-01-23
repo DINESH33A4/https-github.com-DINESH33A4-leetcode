b4d2c9da50d3b86a34baf46099367eac8efecc35
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-23 12:05:33 +0530

    Time: 800 ms (5.04%) | Memory: 66.8 MB (5.14%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxLength(List<String> arr) {
+        HashMap<String, Integer> m = new HashMap<>();
+        return helper(0, arr, "", m);
+    }
+    private static int helper(int idx, List<String>arr, String dup,HashMap<String,Integer> m){
+        if(idx == arr.size())
+            return check(dup)?dup.length():0;
+        if(m.containsKey(idx+dup))
+            return m.get(idx+dup);
+        m.put(idx+dup, Math.max(helper(idx+1, arr, dup+arr.get(idx), m),
+                                helper(idx+1, arr, dup, m)));
+        return m.get(idx+dup);
+    }
+
+     private static boolean check(String s){
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i<s.length(); i++)
+            if(!set.contains(s.charAt(i)))
+                set.add(s.charAt(i));
+            else
+                return false;
+        return true;
+    }
+}
