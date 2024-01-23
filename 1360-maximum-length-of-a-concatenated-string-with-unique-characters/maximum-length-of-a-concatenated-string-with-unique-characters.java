dfe0d02d56053c7bfa87b3845428683871abbe59
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-23 12:33:03 +0530

    Time: 566 ms (5.04%) | Memory: 67.3 MB (5.14%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,22 +1,33 @@
 class Solution {
     public int maxLength(List<String> arr) {
-        HashMap<String, Integer> m = new HashMap<>();
-        return helper(0, arr, "", m);
-    }
-    private static int helper(int idx, List<String>arr, String dup,HashMap<String,Integer> m){
-        if(idx == arr.size())
-            return check(dup)?dup.length():0;
-        if(m.containsKey(idx+dup))
-            return m.get(idx+dup);
-        m.put(idx+dup, Math.max(helper(idx+1, arr, dup+arr.get(idx), m),
-                                helper(idx+1, arr, dup, m)));
-        return m.get(idx+dup);
+        HashMap<String, Integer> memo = new HashMap<>();
+        return helper(0, arr, new StringBuilder(), memo);
     }
 
-     private static boolean check(String s){
+    private static int helper(int idx, List<String> arr, StringBuilder dup, HashMap<String, Integer> memo) {
+        if (idx == arr.size())
+            return check(dup.toString()) ? dup.length() : 0;
+
+        String key = idx + dup.toString();
+        if (memo.containsKey(key))
+            return memo.get(key);
+
+       
+        int includeCurrent = helper(idx + 1, arr, dup.append(arr.get(idx)), memo);
+        dup.setLength(dup.length() - arr.get(idx).length());
+
+        int excludeCurrent = helper(idx + 1, arr, dup, memo);
+
+        int maxLength = Math.max(includeCurrent, excludeCurrent);
+        memo.put(key, maxLength);
+
+        return maxLength;
+    }
+
+    private static boolean check(String s) {
         HashSet<Character> set = new HashSet<>();
-        for(int i = 0; i<s.length(); i++)
-            if(!set.contains(s.charAt(i)))
+        for (int i = 0; i < s.length(); i++)
+            if (!set.contains(s.charAt(i)))
                 set.add(s.charAt(i));
             else
                 return false;
