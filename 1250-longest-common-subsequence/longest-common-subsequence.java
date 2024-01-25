8fa7c4defe792b78863fd4de73f8c1e59df066e2
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-01-25 09:37:02 +0530

    Time: 19 ms (90.91%) | Memory: 45.2 MB (92.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,10 +2,10 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
-        int[] curr = new int[n+1];
         int[] prev = new int[n+1];
-        for( int i = 1; i<=m; i++){
-            curr = new int[n+1];
+        int[] curr = new int[n+1];
+        for(int i = 1; i<=m; i++){
+            curr  = new int[n+1];
             for(int j = 1; j<=n; j++){
                 if(text1.charAt(i-1) == text2.charAt(j-1))
                     curr[j] = 1 + prev[j-1];
@@ -14,35 +14,22 @@ class Solution {
             }
             prev = curr;
         }
-        return prev[n];
+        return curr[n];
+
     }
-
-    //     int[][] dp = new int[m+1][n+1];
-    //     for(int i = 1; i<=m; i++){
-    //         for(int j = 1; j<=n; j++){
-    //             if(text1.charAt(i-1) == text2.charAt(j-1))
-    //                 dp[i][j] = 1 + dp[i-1][j-1];
-    //             else
-    //                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-    //         }
-    //     }
-    //     return dp[m][n];
-    // }
-
-
-    //     for(int[] i : dp)y
+    //     for(int[] i : dp)   
     //         Arrays.fill(i, -1);
-    //     return helper(m-1, n-1, text1, text2, dp);
+    //     return lis(m-1, n-1, text1, text2, dp);
     // }
-
-    // private static int helper(int m, int n, String t1, String t2, int[][] dp){
-    //     if(m<0||n<0)
+    // private static int lis(int i , int j, String text1, String text2, int[][] dp){
+    //     if(i<0 || j<0)
     //         return 0;
-    //     if(dp[m][n] != -1)
-    //         return dp[m][n];
-    //     if(t1.charAt(m)==t2.charAt(n))
-    //         return dp[m][n] = 1 + helper(m-1, n-1, t1, t2, dp);
-    //     else 
-    //         return dp[m][n] = Math.max(helper(m-1, n, t1, t2, dp), helper(m, n-1, t1, t2, dp));
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if(text1.charAt(i) == text2.charAt(j))
+    //         dp[i][j] = 1 + lis(i-1, j-1, text1, text2, dp);
+    //     else
+    //         dp[i][j] = Math.max(lis(i-1, j, text1, text2, dp), lis(i, j-1, text1, text2, dp));
+    //     return dp[i][j];
     // }
 }
