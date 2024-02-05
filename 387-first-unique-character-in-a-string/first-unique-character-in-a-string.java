4ab30a42175e04166147e1dde2d22b65df75a37f
DIVITE DINESH <129586166+DINESH33A4@users.noreply.github.com>
2024-02-05 08:20:09 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (72.81%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,13 +1,21 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int[] map = new int[26];
-        for(int i = s.length() -1; i>=0; i--){
-            map[s.charAt(i) - 'a']++;
+        int ans = Integer.MAX_VALUE;
+        for(int i = 'a'; i<='z'; i++){
+            int x = s.indexOf(i);
+            if(x!=-1 && s.lastIndexOf(i) == x)
+                ans = Math.min(ans, x);
         }
-        for(int i = 0; i<s.length(); i++){
-            if(map[s.charAt(i) - 'a'] == 1)
-                return i;
-        }
-        return -1;
+        return ans == Integer.MAX_VALUE?-1:ans;
+
+        // int[] map = new int[26];
+        // for(int i = s.length() -1; i>=0; i--){
+        //     map[s.charAt(i) - 'a']++;
+        // }
+        // for(int i = 0; i<s.length(); i++){
+        //     if(map[s.charAt(i) - 'a'] == 1)
+        //         return i;
+        // }
+        // return -1;
     }
 }
